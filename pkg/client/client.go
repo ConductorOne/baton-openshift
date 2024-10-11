c000812960ff345f6b0748e4e5e7325b0a36dbca
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-10-11 14:29:29 -0600

    Add k8s client

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -2,26 +2,32 @@ package client
 
 import (
 	"context"
+	"fmt"
 
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	userv1 "github.com/openshift/client-go/user/clientset/versioned/typed/user/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
 type Client struct {
 	config      *rest.Config
 	usersClient *userv1.UserV1Client
+	k8sClient   *kubernetes.Clientset
 }
 
 func New(c *rest.Config) (*Client, error) {
 	usrc, err := userv1.NewForConfig(c)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("unable to create UserV1Client client, error: %w", err)
+	}
+	k8sc, err := kubernetes.NewForConfig(c)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create k8s client, error: %w", err)
 	}
 
-	return &Client{usersClient: usrc, config: c}, nil
-}
+	return &Client{config: c, usersClient: usrc, k8sClient: k8sc}, nil
 }
 
 // ListUsers list the users of the Openshift cluster
