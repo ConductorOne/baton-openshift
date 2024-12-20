514ceadeb577b3270f395981d05334624a587e99
Jorge Javier Araya Navarro <jorge@esavara.cr>
2024-12-20 14:16:26 -0600

    Synchronize the nix flake package with the git tag

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
         {
           default = pkgs.buildGoModule {
             pname = "baton-openshift";
-            version = "0.1.0";
+            version = "0.0.1";
 
             src = ./.;
 
