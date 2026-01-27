458145dd822801363e61ef483454adc6d71606e2
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:29:01 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.2

--- cmd/baton-openshift/main.go
+++ cmd/baton-openshift/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/conductorone/baton-openshift/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
+	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/types"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
@@ -29,6 +30,7 @@ func main() {
 		field.Configuration{
 			Fields: ConfigurationFields,
 		},
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Connector{}),
 	)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
